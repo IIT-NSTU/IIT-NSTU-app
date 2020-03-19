c7b6beac597bb36cb9883c8a643294b3524737d8
arnabdey24 <58462502+arnabdey24@users.noreply.github.com>
2020-03-19 18:37:43 +0600

    19/03/20 2:16 am splash activity working fine!

--- app/src/main/java/com/example/iitnstu/MainActivity.java
+++ app/src/main/java/com/example/iitnstu/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.iitnstu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+         //      WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setStatusBarColor(this.getResources().getColor(R.color.white));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },3000);
 
     }
 }
