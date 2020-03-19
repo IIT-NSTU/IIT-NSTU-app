1ff798be69c775e7ec77c072b23a0b7f0941e871
arnabdey24 <58462502+arnabdey24@users.noreply.github.com>
2020-03-20 02:16:40 +0600

    3/20/20 2.16pm Introduction added.

--- app/src/main/java/com/example/iitnstu/HomeActivity.java
+++ app/src/main/java/com/example/iitnstu/HomeActivity.java
@@ -2,9 +2,9 @@ package com.example.iitnstu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,6 +12,11 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onClick(View view){
+        Intent intent=new Intent(HomeActivity.this,IntroActivity.class);
+        startActivity(intent);
     }
 
 }
