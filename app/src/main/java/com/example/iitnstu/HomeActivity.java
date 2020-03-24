eaa1a32899e7341ae4cb916c3a48afe7c03d4474
NAIMUR <nirzon.naim@gmail.com>
2020-03-24 11:43:17 +0600

    11:42 AM 24/03/2020
    (Added PDF file for Syllabus)
    (Added new Button for PDF View)

--- app/src/main/java/com/example/iitnstu/HomeActivity.java
+++ app/src/main/java/com/example/iitnstu/HomeActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/iitnstu/SyllabusActivity.java
+++ app/src/main/java/com/example/iitnstu/SyllabusActivity.java
@@ -1,16 +1,33 @@
 package com.example.iitnstu;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SyllabusActivity extends AppCompatActivity {
+
+    Button View_Syllabus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_syllabus);
+
+        View_Syllabus = (Button) findViewById(R.id.button);
+
+        View_Syllabus.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent i = new Intent (SyllabusActivity.this, Pdf_Syllabus_Activity.class);
+                startActivity(i);
+
+            }
+
+
+        });
     }
 
 
