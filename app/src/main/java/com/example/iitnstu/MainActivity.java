147f7b84bbe4b5fa598f061ca73ff8a99c4fb499
arnabdey24 <58462502+arnabdey24@users.noreply.github.com>
2020-03-26 16:04:27 +0600

    3/26/20 4.03pm some issue in merging fixed.

--- app/src/main/java/com/example/iitnstu/MainActivity.java
+++ app/src/main/java/com/example/iitnstu/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 
 
 public class MainActivity extends AppCompatActivity {
