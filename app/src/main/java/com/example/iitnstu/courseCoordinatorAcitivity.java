fbe82f757ba5a0a6b00482268e5d1cd2d0f25873
NAIMUR <nirzon.naim@gmail.com>
2020-03-31 13:30:21 +0600

    01:29 PM 31/03/2020
    (Modified Course Coordinator's info)!

--- app/src/main/java/com/example/iitnstu/courseCoordinatorAcitivity.java
+++ app/src/main/java/com/example/iitnstu/courseCoordinatorAcitivity.java
@@ -3,6 +3,10 @@ package com.example.iitnstu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
 
 public class courseCoordinatorAcitivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class courseCoordinatorAcitivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_coordinator_acitivity);
+
+
+        TextView email = (TextView) findViewById(R.id.TextViewEmailDipa);
+        email.setText(Html.fromHtml("<a href=\"mailto:dipa.iit.nstu@gmail.com\">dipa.iit.nstu@gmail.com</a>" ));
+        email.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView email2 = (TextView) findViewById(R.id.TextViewEmailSumon);
+        email2.setText(Html.fromHtml("<a href=\"mailto:auhidsumon@gmail.com\">auhidsumon@gmail.com</a>" ));
+        email2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView email3 = (TextView) findViewById(R.id.TextViewEmailFalguni);
+        email3.setText(Html.fromHtml("<a href=\"mailto:falguni.iit@gmail.com\">falguni.iit@gmail.com</a>" ));
+        email3.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 }
