4b087d3ba0c072f85c44496681768fe146b95f32
Push <sunaansultaniit@gmail.com>
2020-04-01 20:44:12 +0600

    1/4/2020 added pictures

---
+++ app/src/main/java/com/example/iitnstu/Cards3.java
@@ -0,0 +1,14 @@
+package com.example.iitnstu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cards3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards3);
+    }
+}
