3d523843b81c9d840740a611906371d410c009f3
arnabdey24 <arnabdey665@gmail.com>
2021-02-06 03:19:57 +0600

    A profile class created and used it to expand teacher profile

--- app/src/main/java/com/whatEver/iitnstu/Profile.java
+++ app/src/main/java/com/whatEver/iitnstu/Profile.java
@@ -3,12 +3,32 @@ package com.whatEver.iitnstu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+
 
 public class Profile extends AppCompatActivity {
+
+    private HashMap<String, Object> tmp;
+    private ImageView profileImage;
+    private TextView profileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        tmp=(HashMap)getIntent().getSerializableExtra("info");
+
+        profileImage=findViewById(R.id.profile_img);
+        profileName=findViewById(R.id.profile_name);
+
+        Picasso.get().load(tmp.get("imageLink").toString()).into(profileImage);
+        profileName.setText(tmp.get("name").toString());
+
     }
 }
--- app/src/main/java/com/whatEver/iitnstu/TeachersInfo.java
+++ app/src/main/java/com/whatEver/iitnstu/TeachersInfo.java
@@ -16,12 +16,14 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.Serializable;
 import java.util.HashMap;
 
 public class TeachersInfo extends AppCompatActivity {
 
     private FirebaseFirestore db;
     private GridLayout gridLayout;
+
 
 
 
@@ -37,13 +39,14 @@ public class TeachersInfo extends AppCompatActivity {
         final LoadingDialog loadingDialog=new LoadingDialog(TeachersInfo.this);
         loadingDialog.startLoadingDialog();
 
+
         //if(Connection.check(context))
             db.collection("teachers").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if(task.isSuccessful()){
                     for (DocumentSnapshot data: task.getResult()) {
-                        HashMap<String, Object> tmp = (HashMap<String, Object>) data.getData();
+                        final HashMap<String, Object> tmp = (HashMap<String, Object>) data.getData();
                         TeacherCard teacherCard = new TeacherCard(context, tmp.get("name").toString(),
                                 tmp.get("designation").toString(), tmp.get("phone").toString(),
                                 tmp.get("email").toString(), tmp.get("imageLink").toString());
@@ -51,8 +54,7 @@ public class TeachersInfo extends AppCompatActivity {
                         teacherCard.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-                                startActivity(new Intent(TeachersInfo.this,Profile.class));
-                                finish();
+                                startActivity(new Intent(TeachersInfo.this,Profile.class).putExtra("info",tmp));
                             }
                         });
 
