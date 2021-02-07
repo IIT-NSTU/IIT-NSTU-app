d0a657c9278e37b8831711b783bce51874a79ac2
arnabdey24 <arnabdey665@gmail.com>
2021-02-07 16:50:55 +0600

    profile fixed

--- app/src/main/java/com/whatEver/iitnstu/Cards.java
+++ app/src/main/java/com/whatEver/iitnstu/Cards.java
@@ -32,7 +32,7 @@ public class Cards extends FrameLayout {
         setLayoutParams(layoutParams);
 
         Animation animation = AnimationUtils.loadAnimation(context, R.anim.fromdown);
-        CardView cardView=findViewById(R.id.card);
+        CardView cardView=findViewById(R.id.education_info);
         cardView.startAnimation(animation);
 
         ImageView studentPic=findViewById(R.id.studentPic);
--- app/src/main/java/com/whatEver/iitnstu/Profile.java
+++ app/src/main/java/com/whatEver/iitnstu/Profile.java
@@ -3,7 +3,9 @@ package com.whatEver.iitnstu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -15,7 +17,8 @@ public class Profile extends AppCompatActivity {
 
     private HashMap<String, Object> tmp;
     private ImageView profileImage;
-    private TextView profileName,profileWork;
+    private TextView profileName,profileWork,educationInfo,educationInfo2;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +26,28 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         tmp=(HashMap)getIntent().getSerializableExtra("info");
+        HashMap<String,String> educationInfo=(HashMap) tmp.get("educationInfo");
+
+        Log.d("debug",educationInfo.toString());
 
         profileImage=findViewById(R.id.profile_img);
         profileName=findViewById(R.id.profile_name);
         profileWork=findViewById(R.id.profile_work);
+        linearLayout=findViewById(R.id.profile_edu_card);
+        //educationInfo=findViewById(R.id.education_info);
+        //educationInfo2=findViewById(R.id.education_info2);
 
         Picasso.get().load(tmp.get("imageLink").toString()).into(profileImage);
         profileName.setText(tmp.get("name").toString());
         profileWork.setText(tmp.get("designation").toString());
+        //educationInfo.setText(tmp.get("educationInfo").toString());
+        //educationInfo2.setText(tmp.get("educationInfo2").toString());
+
+        for (String s : educationInfo.keySet()) {
+            TextView textView=new TextView(getApplicationContext());
+            textView.setText("\n"+s+" : "+educationInfo.get(s));
+            linearLayout.addView(textView);
+        }
 
 
 
